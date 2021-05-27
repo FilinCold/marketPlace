b9dfa6804a142a44a1d104930998109bee17f642
Filin <proskurkinv@mail.ru>
2021-05-27 10:49:54 +0300

    Update files

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import icon from '../../../img/image 24.png';
 import girl from '../../../img/girl.svg';
-import rec from '../../../img/Rectangle 122 (1).png';
+import square from '../../../img/square.svg'
 
 const Card = (props) => {
 
@@ -14,10 +14,17 @@ const Card = (props) => {
           <div className='rectangle'>
             <img className='user_icon' src={girl} alt=""/>
           </div>
-
+          <div className='rectangle2'>
+            <img className='user_icon2' src={square} alt=""/>
+            <span>3/5</span>
+          </div>
+          <div className="type-card">
+            <div className='rectangle3'>
+              Alimation Character
+            </div>
+          </div>
         </div>
-        <div className="count-img"></div>
-        <div className="type-card"></div>
+
       </div>
       <div className="author">by Amy</div>
 
